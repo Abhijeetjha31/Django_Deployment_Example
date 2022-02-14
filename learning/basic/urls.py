a5192f754400c4d83273a1cae66dae94508897ea
Abhijeet Jha <abhijeet31082002@gmail.com>
2022-02-14 12:58:19 +0530

    first

---
+++ learning/basic/urls.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.urls import path
+from basic import views
+
+app_name='basic'
+
+urlpatterns=[
+    path('register/',views.register,name="register"),
+    path('login/',views.user_login,name="user_login")
+]
